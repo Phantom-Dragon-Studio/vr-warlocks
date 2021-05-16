776dc3fd3e30e78a688970d3d9ab75d21c873430
Casey <fullme7aljacke7@gmail.com>
2021-05-16 11:40:42 -0400

    Initial Commit

---
+++ Assets/Phantom Dragon Studio/Dynamic Spell System/MagicProjectileSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class MagicProjectileSpawner : MonoBehaviour
+{
+    [SerializeField] private Settings _settings;
+
+    public void CreateMagicBolt(Vector3 spawnPoint, Quaternion spawnRotation)
+    {
+        Instantiate(_settings.myData.SFX, spawnPoint, spawnRotation, null);
+    }
+
+    [Serializable]
+    public class Settings
+    {
+        [SerializeField] public ProjectileData myData;
+    }
+}
---
+++ Assets/Phantom Dragon Studio/Dynamic Spell System/ProjectileData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Projectile Data", menuName = "New Projectile Data", order = 0)]
+
+[System.Serializable]
+public class ProjectileData : ScriptableObject
+{
+    [SerializeField] new string name;
+    [SerializeField] Sprite icon;
+    [SerializeField] GameObject sfx;
+   // [SerializeField] private Element element;
+    [SerializeField] private float damage;
+    [SerializeField] private float lifetime;
+    [SerializeField] private float speed;
+
+    public float Speed { get => speed; private set => speed = value; }
+    public float Lifetime { get => lifetime; private set => lifetime = value; }
+    public float Damage { get => damage; private set => damage = value; }
+    //public Element Element { get => element; private set => element = value; }
+    public GameObject SFX { get => sfx; private set => sfx = value; }
+    public Sprite Icon { get => icon; private set => icon = value; }
+    public string Name { get => name; private set => name = value; }
+}
